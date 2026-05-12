e55a6a6b7fc560b120494fb27918101b0a0ff14b
thegodgg <63165909+thegodgg@users.noreply.github.com>
2026-05-12 19:34:34 -0400

    test

---
+++ ms-productos/src/main/java/com/duoc/ms_productos/controller/ProductoController.java
@@ -0,0 +1,30 @@
+﻿package com.duoc.ms_productos.controller;
+
+import com.duoc.ms_productos.model.Producto;
+import com.duoc.ms_productos.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public List<Producto> listar() {
+        return productoService.listarTodos();
+    }
+
+    @PostMapping
+    public Producto crear(@RequestBody Producto producto) {
+        return productoService.guardar(producto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        productoService.eliminar(id);
+    }
+}
---
+++ ms-productos/src/main/java/com/duoc/ms_productos/service/ProductoService.java
@@ -0,0 +1,26 @@
+﻿package com.duoc.ms_productos.service;
+
+import com.duoc.ms_productos.model.Producto;
+import com.duoc.ms_productos.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> listarTodos() {
+        return productoRepository.findAll();
+    }
+
+    public Producto guardar(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    public void eliminar(Long id) {
+        productoRepository.deleteById(id);
+    }
+}
